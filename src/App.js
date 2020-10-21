04b44e56b40df50d7ecc8466e319d445cdc08a4b
bostalowski <bastien.ostalowski@lunc.hr>
2020-10-21 15:45:13 +0200

    test-amplify-preview

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload part 2.
+          Edit <code>src/App.js</code> and save to reload.
+          Test amplify preview
         </p>
         <a
           className="App-link"
